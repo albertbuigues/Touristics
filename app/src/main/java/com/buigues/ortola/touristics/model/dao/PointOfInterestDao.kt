472291266cadabb3a1ef908d4ea2a83ce581d62c
Albert <albertbuigues@gmail.com>
2021-10-22 15:04:30 +0200

    Routes inserted TODO: Fix Points not inserted to Room

--- app/src/main/java/com/buigues/ortola/touristics/model/dao/PointOfInterestDao.kt
+++ app/src/main/java/com/buigues/ortola/touristics/model/dao/PointOfInterestDao.kt
@@ -3,15 +3,12 @@ package com.buigues.ortola.touristics.model.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy.IGNORE
-import androidx.room.Query
+import com.buigues.ortola.touristics.model.entity.PointOfInterest
 import com.buigues.ortola.touristics.model.entity.Route
 
 @Dao
-interface RouteDao
-{
-    @Insert(onConflict = IGNORE)
-    suspend fun insertRoute(route: Route)
+interface PointOfInterestDao {
 
-    @Query("SELECT * FROM routes_tbl")
-    fun getAllRoutes(): List<Route>
+    @Insert(onConflict = IGNORE)
+    suspend fun insertPointByRoute(route: Route, point: PointOfInterest)
 }
--- app/src/main/java/com/buigues/ortola/touristics/repository/FirebaseRepository.kt
+++ app/src/main/java/com/buigues/ortola/touristics/repository/FirebaseRepository.kt
@@ -2,6 +2,7 @@ package com.buigues.ortola.touristics.repository
 
 import android.app.Application
 import com.buigues.ortola.touristics.model.database.AppDatabase
+import com.buigues.ortola.touristics.model.entity.PointOfInterest
 import com.buigues.ortola.touristics.model.entity.Route
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.coroutines.CoroutineScope
@@ -31,8 +32,33 @@ class FirebaseRepository @Inject constructor(private val application: Applicatio
                 historicPeriod = dataMap["historicPeriod"].toString(),
                 imageUrl = dataMap["imageUrl"].toString()
             )
+            getRoutePoints(route,"0")
+            getRoutePoints( route,"1")
             CoroutineScope(Dispatchers.IO).launch {
                 AppDatabase.getInstance(application).routeDao.insertRoute(route)
+            }
+        }
+    }
+
+    private fun getRoutePoints(route: Route, routeId: String) {
+        val dbReference = FirebaseDatabase.getInstance().getReference("/routes/$routeId")
+        dbReference.child("pointsOfInterest").get().addOnSuccessListener { pointsArray ->
+            pointsArray.children.forEach { point ->
+                val dataMap = point.value as Map<*, *>
+                val routeIdToInsert = routeId.toInt()
+                val id = point.key!!.toInt()
+                val pointOfInterest = PointOfInterest(
+                    id = id,
+                    title = dataMap["title"].toString(),
+                    description = dataMap["description"].toString(),
+                    latitude = dataMap["latitude"].toString().toDouble(),
+                    longitude = dataMap["longitude"].toString().toDouble(),
+                    imageUrl = dataMap["imageUrl"].toString(),
+                    routeId = routeIdToInsert
+                )
+                CoroutineScope(Dispatchers.IO).launch {
+                    AppDatabase.getInstance(application).pointDao.insertPointByRoute(route, pointOfInterest)
+                }
             }
         }
     }
