51b9cb0d1a0d6d5ef3f8d8c9c79d02909fe1ef8c
Albert <albertbuigues@gmail.com>
2021-11-01 13:44:05 +0100

    TODO: Adapter not binding info when play

--- app/src/main/java/com/buigues/ortola/touristics/ui/MainActivity.kt
+++ app/src/main/java/com/buigues/ortola/touristics/ui/MainActivity.kt
@@ -2,18 +2,11 @@ package com.buigues.ortola.touristics.ui
 
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.annotation.UiThread
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.buigues.ortola.touristics.databinding.ActivityMainBinding
-import com.buigues.ortola.touristics.model.dao.RouteDao
-import com.buigues.ortola.touristics.model.entity.Route
-import com.buigues.ortola.touristics.model.repository.RoomRepository
 import com.buigues.ortola.touristics.viewmodel.RoutesListViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
